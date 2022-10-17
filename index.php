2a8655f7ca62736af2dd48c184cad9af1c5eba9c
geovannaruy2 <116040342+geovannaruy2@users.noreply.github.com>
2022-10-17 19:34:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Média PHP </title>
+</head>
+<body>
+    <form action="media.php" method="POST">
+        Insira nota 1: <input type="text" nome = "nota1"><br>
+        Insira nota 2: <input type="text" nome = "nota1"><br>
+        Insira nota 3: <input type="text" nome = "nota1"><br>
+
+        <input type="submit">
+</body>
+</html>
